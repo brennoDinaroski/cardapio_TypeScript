73927e106c4b18fd8c3819de2f451f9d70e349a9
brennoDinaroski <brennno.dinaroski@gmail.com>
2024-02-01 17:29:01 -0300

    Populating items with json cardapio informations

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,30 +1,34 @@
 import styles from './Item.module.scss'
 import logo from 'assets/logo.svg'
+import cardapio from '../itens.json'
 
-export default function Item() {
+type Props = typeof cardapio[0]
+
+export default function Item(props : Props) {
+    const {title, description, category, size, serving, price} = props
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt='Imagem' ></img>
+                <img src={logo} alt={title} ></img>
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
-                    <h2>Macarrão</h2>
-                    <p>Descrição do macarrão</p>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
                 </div>
             </div>
             <div className={styles.item__tags}>
                 <div className={styles.item__tipo}>
-                    Tipo: Massa
+                    {category.label}
                 </div>
                 <div className={styles.item__porcao}>
-                    Porcao: 400 g
+                    {size} g
                 </div>
                 <div className={styles.item__qtdpessoas}>
-                    Serve 2 pessoas
+                    Serve {serving} pessoa{serving === 1 ? "" : "s"}
                 </div>
                 <div className={styles.item__valor}>
-                    Valor R$ 50
+                    R$ {price.toFixed(2)}
                 </div>
             </div>
         </div>
