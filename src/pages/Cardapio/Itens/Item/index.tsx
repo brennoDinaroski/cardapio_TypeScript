838335bb2099e154dedd14bb66e55087682085e4
brennoDinaroski <brennno.dinaroski@gmail.com>
2024-02-01 01:52:54 -0300

    Make Item and Itens, patterns in react and scss to each one

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,9 +1,32 @@
-// 6 min class Criando Itens
+import styles from './Item.module.scss'
+import logo from 'assets/logo.svg'
 
 export default function Item() {
     return (
-        <div>
-            Item
+        <div className={styles.item}>
+            <div className={styles.item__imagem}>
+                <img src={logo} alt='Imagem' ></img>
+            </div>
+            <div className={styles.item__descricao}>
+                <div className={styles.item__titulo}>
+                    <h2>Macarrão</h2>
+                    <p>Descrição do macarrão</p>
+                </div>
+            </div>
+            <div className={styles.item__tags}>
+                <div className={styles.item__tipo}>
+                    Tipo: Massa
+                </div>
+                <div className={styles.item__porcao}>
+                    Porcao: 400 g
+                </div>
+                <div className={styles.item__qtdpessoas}>
+                    Serve 2 pessoas
+                </div>
+                <div className={styles.item__valor}>
+                    Valor R$ 50
+                </div>
+            </div>
         </div>
     )
 }
