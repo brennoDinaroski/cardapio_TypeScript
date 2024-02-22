1f4ad323d2f25b0ed33384aa02387d959b55b448
brennoDinaroski <brenno.dinaroski@gmail.com>
2024-02-22 01:34:21 -0300

    cleaned index

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
 import Router from 'routes.js';
-
-/* const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />; */
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
